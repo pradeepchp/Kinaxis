232508ea6279e72074fe658e6d86ed49ebce8ff7
Marc27 <marc27@Marc27s-MacBook-Air.local>
2023-02-02 10:09:01 +0530

    updated g2 function

--- Modules/cloud_functions_g2/main.tf
+++ Modules/cloud_functions_g2/main.tf
@@ -1,6 +1,6 @@
 resource "google_storage_bucket" "source-bucket" {
   name     = var.bucket_name
-  location = var.region
+  location = var.location
   uniform_bucket_level_access = true
 }
 
@@ -10,14 +10,64 @@ resource "google_storage_bucket_object" "object" {
   source = var.src_obj_path  # Add path to the zipped function source code
 }
 
+resource "google_storage_bucket" "trigger-bucket" {
+  name     = "gcf-trigger-bucket"
+  location = var.location # The trigger must be in the same location as the bucket
+  uniform_bucket_level_access = true
+}
+
+# iam and service account
+data "google_storage_project_service_account" "gcs_account" {
+}
+
+resource "google_project_iam_member" "gcs-pubsub-publishing" {
+  project = var.project-id
+  role    = "roles/pubsub.publisher"
+  member  = "serviceAccount:${data.google_storage_project_service_account.gcs_account.email_address}"
+}
+
+resource "google_service_account" "account" {
+  account_id   = "gcf-sa"
+  display_name = "Test Service Account - used for both the cloud function and eventarc trigger in the test"
+}
+
+# Permissions on the service account used by the function and Eventarc trigger
+resource "google_project_iam_member" "invoking" {
+  project = var.project-id
+  role    = "roles/run.invoker"
+  member  = "serviceAccount:${google_service_account.account.email}"
+  depends_on = [google_project_iam_member.gcs-pubsub-publishing]
+}
+
+resource "google_project_iam_member" "event-receiving" {
+  project = var.project-id
+  role    = "roles/eventarc.eventReceiver"
+  member  = "serviceAccount:${google_service_account.account.email}"
+  depends_on = [google_project_iam_member.invoking]
+}
+
+resource "google_project_iam_member" "artifactregistry-reader" {
+  project = var.project-id
+  role     = "roles/artifactregistry.reader"
+  member   = "serviceAccount:${google_service_account.account.email}"
+  depends_on = [google_project_iam_member.event-receiving]
+}
+# cloud function conf
 resource "google_cloudfunctions2_function" "function" {
+  depends_on = [
+    google_project_iam_member.event-receiving,
+    google_project_iam_member.artifactregistry-reader,
+  ]
   name = var.function_name
-  location = var.region
+  location = var.location
   description = var.funtion_description
 
   build_config {
-    runtime = var.runtime
-    entry_point = var.function_entry_point  # Set the entry point
+    runtime     = var.runtime
+    entry_point = var.function_entry_point
+    environment_variables = {
+      BUILD_CONFIG_TEST = "build_test"
+    }
     source {
       storage_source {
         bucket = google_storage_bucket.source-bucket.name
@@ -27,12 +77,26 @@ resource "google_cloudfunctions2_function" "function" {
   }
 
   service_config {
-    max_instance_count  = 1
+    max_instance_count  = 3
+    min_instance_count = 1
     available_memory    = "256M"
     timeout_seconds     = 60
+    environment_variables = {
+      SERVICE_CONFIG_TEST = "config_test"
+    }
+    ingress_settings = "ALLOW_INTERNAL_ONLY"
+    all_traffic_on_latest_revision = true
+    service_account_email = google_service_account.account.email
   }
-}
 
-output "function_uri" {
-  value = google_cloudfunctions2_function.function.service_config[0].uri
+  event_trigger {
+    trigger_region = "us-central1" # The trigger must be in the same location as the bucket
+    event_type = "google.cloud.storage.object.v1.finalized"
+    retry_policy = "RETRY_POLICY_RETRY"
+    service_account_email = google_service_account.account.email
+    event_filters {
+      attribute = "bucket"
+      value = google_storage_bucket.trigger-bucket.name
+    }
+  }
 }
