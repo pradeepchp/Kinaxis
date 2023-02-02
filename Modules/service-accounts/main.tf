18758dfc5e6310f7f1f2ac278e39266b37bf4275
pradeepchp <115686365+pradeepchp@users.noreply.github.com>
2023-02-02 19:11:23 +0530

    Delete Modules directory

--- Modules/service-accounts/main.tf
+++
@@ -1,9 +0,0 @@
-resource "google_service_account" "test-acc" {
-  account_id   = "test-acc-sa2"
-  display_name = "Example Service Account"
-}
-resource "google_project_iam_member" "test-acc" {
-  project = var.project-id
-  role    = "roles/viewer"
-  member  = "serviceAccount:${google_service_account.test-acc.email}"
-}
